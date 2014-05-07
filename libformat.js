aad7a955fadd6a708a03658ace5b79043cd6a906
Mark Safronov <hijarian@gmail.com>
2014-05-07 18:50:13 +0400

    Выделил конкретный API для очистки входных строк

--- libformat.js
+++ libformat.js
@@ -2,8 +2,18 @@
  * Rules for formatting stuff.
  */
 
-String.prototype.ucfirst = function () {
-    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+String.prototype.titleCaseAll = function () {
+    return this
+        .replace(
+                /[a-zA-Z\u0400-\u04FF]+/g,  // Cyrillic letters and Latin letters
+            function(txt){
+                return txt.titleCase();
+            }
+        );
+}
+
+String.prototype.titleCase = function () {
+    return this.charAt(0).toUpperCase() + this.substr(1).toLowerCase();
 };
 
 String.prototype.trim = function () {
@@ -12,7 +22,83 @@ String.prototype.trim = function () {
         .replace(/\s+$/, '');
 }
 
-exports.formatSurname = function (raw) {
-    return raw.trim().ucfirst();
+String.prototype.collapseSpaces = function () {
+    return this
+        .replace(/ +/g, ' ');
 }
 
+String.prototype.correctSpacesAroundParentheses = function () {
+    return this
+        .replace(/\s*\(\s*/g, ' (')
+        .replace(/\s*\)\s*/g, ') ');
+};
+
+String.prototype.correctSpacesAroundPunctuation = function () {
+    return this
+        .replace(/\s*([,.;:!?])\s*/g, '$1 ')
+        .replace(/\. :/g, '.:');
+}
+
+//------------------------------------------------------------------------
+// API
+function trim(raw) {
+    return raw.trim();
+}
+
+function clean(raw) {
+    return raw.collapseSpaces();
+}
+
+function titlecase(raw) {
+    return raw.titleCaseAll();
+}
+
+function punctuate(raw) {
+    return raw
+        .correctSpacesAroundPunctuation()
+        .correctSpacesAroundParentheses()
+        .trim();
+}
+
+function phoneformat(raw) {
+
+    // If we have short number, there's nothing really what we can do.
+    if (raw.length < 10)
+        return raw;
+
+    // Basic cleanup of the long number
+    var base = raw
+        .collapseSpaces()
+        .correctSpacesAroundParentheses()
+        .trim();
+
+    // If we have parentheses, GREAT! Remove everything around them and put 8 at start.
+    base = base.replace(/.*\((.*)\)[^0-9]+/, '8 ($1) ');
+    
+    // mobile phone without 8
+    if (base.charAt(0) == '9' || base.substring(0, 2) == '(9') 
+        return base.replace(/[^0-9]/g, '').replace(/(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '+7 ($1) $2-$3-$4');
+
+    // mobile number without '+' and punctuation
+    if (base.substring(0,2) == '79')
+        return base.substring(1).replace(/[^0-9]/g, '').replace(/(\d\d\d)(\d\d\d)(\d\d)(\d\d)$/, '8 ($1) $2-$3-$4').replace(/^8 \(9/, '+7 (9');
+
+    // Replace zone code in dashes with zone code in parentheses
+    base = base.replace(/^8-(\d+)-/, '8 ($1) ');
+
+    // If zone code starts with 9, it's mobile number. Start it with international +7 
+    // instead of Russia-specific 8
+    base = base.replace(/^8 \(9/, '+7 (9');
+
+    return base;
+}
+
+
+//------------------------------------------------------------------------
+module.exports = {
+    trim: trim,
+    clean: clean,
+    titlecase: titlecase,
+    punctuate: punctuate,
+    phoneformat: phoneformat
+}
--- libtest.js
+++ libtest.js
@@ -1,36 +1,158 @@
 var lib = require('./libformat.js');
 
-var surname = function (input, expected) {
-   lib.formatSurname(input).should.equal(expected); 
-};
-
 describe('lib', function () {
-    describe('convertSurname()', function () {
+
+    describe('trim()', function () {
+        var trim = function (input, expected) {
+            lib.trim(input).should.equal(expected);
+        };
+
         it('should return empty string for empty input', function () {
-            surname("", "");
+            trim("", "");
+        });
+        it("should return same string if it's all alphanumeric", function () {
+            trim("Иванов", "Иванов");
         });
         it('should trim spaces and newlines from string', function () {
-            surname("Surname", "Surname");
-            surname(" Surname", "Surname");
-            surname("  Surname", "Surname");
-            surname("Surname ", "Surname");
-            surname("Surname  ", "Surname");
-            // Now with Cyrillic
-            surname("Фамилия", "Фамилия");
-            surname(" Фамилия", "Фамилия");
-            surname("  Фамилия", "Фамилия");
-            surname("Фамилия ", "Фамилия");
-            surname("Фамилия  ", "Фамилия")
-            surname("\nФамилия  ", "Фамилия")
-            surname("\r\nФамилия \r ", "Фамилия")
+            trim("Фамилия", "Фамилия");
+            trim(" Фамилия", "Фамилия");
+            trim("  Фамилия", "Фамилия");
+            trim("Фамилия ", "Фамилия");
+            trim("Фамилия  ", "Фамилия")
+            trim("\nФамилия  ", "Фамилия")
+            trim("\r\nФамилия \r ", "Фамилия")
+            trim("\r Иван \r\n \n   ", "Иван");
+            trim(" ", "");
+            trim("  ", "");
         });
-        it('should uppercase a first letter', function () {
-            surname("фамилия", "Фамилия");
+    });
+
+    describe('clean()', function () {
+        var clean = function (input, expected) {
+            lib.clean(input).should.equal(expected);
+        };
+
+        it('should handle trivial case', function () {
+            clean("", "");
+        });
+        it('should collapse repeating spaces inside strings', function () {
+            clean("Иван     Иванович", "Иван Иванович");
+            clean("Иванов   Иван     Иванович", "Иванов Иван Иванович");
+        });
+    });
+
+    describe('titlecase()', function () {
+        var titlecase = function (input, expected) {
+            lib.titlecase(input).should.equal(expected);
+        };
+        
+        it('should handle trivial case', function () {
+            titlecase("", "");
+        });
+        it('should uppercase a first letter in each word', function () {
+            titlecase("фамилия", "Фамилия");
+            titlecase("иван иванович", "Иван Иванович");
+            titlecase("surname name family-name", "Surname Name Family-Name");
         });
         it('should downcase all letters except first', function () {
-            surname("ФАМИЛИЯ", "Фамилия");
-            surname("фАМИЛИЯ", "Фамилия");
-        })
-    })
+            titlecase("ФАМИЛИЯ", "Фамилия");
+            titlecase("фАМИЛИЯ", "Фамилия");
+        });
+    });
+
+    describe('punctuate()', function () {
+        var punctuate = function (input, expected) {
+            lib.punctuate(input).should.equal(expected);
+        };
+
+        it('should ensure one space after punctuation and no spaces before punctuation', function () {
+            punctuate("ул .Городская", "ул. Городская");
+            punctuate("ул. Городская  , д . 4", "ул. Городская, д. 4");
+            punctuate(",.;:?!a", ", . ; : ? ! a");
+        });
+
+        it('should put no spaces at end of string', function () {
+            punctuate("ул.", "ул.");
+            punctuate("ул . Овчинникова !", "ул. Овчинникова!");
+            punctuate(" ! ", "!");
+        });
+        it('should tolerate multiple spaces', function () {
+            punctuate("ул. Железнодорожников     ,    д   . 88,кв.183", "ул. Железнодорожников, д. 88, кв. 183");
+        });
+        
+        it('should not put a space between a point and a colon', function () {
+            punctuate("моб.:", "моб.:");
+            punctuate("тел . :", "тел.:");
+            punctuate('доб.:41-16', "доб.: 41-16");
+        });
+
+        it('should ensure that there is a space before an opening brackets', function () {
+            punctuate("Один(другой", "Один (другой");
+            punctuate("Один (другой", "Один (другой");
+        });
+        it('should ensure that there is no spaces after an opening brackets', function () {
+            punctuate("Один (другой", "Один (другой");
+            punctuate("Один ( другой", "Один (другой");
+        });
+        it('should ensure that there is no spaces before closing brackets', function () {
+            punctuate("другой)", "другой)");
+            punctuate("другой  )", "другой)");
+        });
+        it('should handle several problems with brackets normally', function () {
+            punctuate("Один ( другой ) сделал ( не сделал )", "Один (другой) сделал (не сделал)");
+        });
+    });
+
+    describe('phoneformat()', function () {
+        var phoneformat = function (input, expected) {
+            return lib.phoneformat(input).should.equal(expected);
+        };
+
+        it('should handle trivial cases', function () {
+            phoneformat("", "");
+        });
+
+        it('should put missing 8 before the number', function () {
+            phoneformat('(495) 967-968-9', '8 (495) 967-968-9');
+            phoneformat('(4812) - 350-973', '8 (4812) 350-973');
+            phoneformat('(4922) 53-15-81', '8 (4922) 53-15-81');
+            phoneformat('(495) 941-93-20', '8 (495) 941-93-20');
+        });
+        
+        it('should put missing +7 before the mobile number', function () {
+            phoneformat('915-295-62-86', '+7 (915) 295-62-86');
+            phoneformat('(915)-295-62-86', '+7 (915) 295-62-86');
+        });
+
+        it('should replace 8 with +7 in mobile numbers', function () {
+            phoneformat('8(981)440-32-01', '+7 (981) 440-32-01');
+        });
+
+        it('should correct improper spaces placement around the zone code and clean up unnecessary symbols', function () {
+            phoneformat('8(495) 685-95-95', '8 (495) 685-95-95');
+            phoneformat('+7(916) 885-11-58', '+7 (916) 885-11-58');
+            phoneformat('+7(953) 60-39-094', '+7 (953) 60-39-094');
+            phoneformat('8 (4812) - 350-973', '8 (4812) 350-973');
+        });
+
+        it('should leave numbers with less than 10 symbols in them as is', function () {
+            phoneformat('27-97-27', '27-97-27');
+            phoneformat('41-16', "41-16");
+            phoneformat('4120', "4120");
+        });
+
+        it('should correct most usual mistakes in writing numbers', function () {
+            phoneformat('79160070695', '+7 (916) 007-06-95');
+            phoneformat('8-905-696-55-91', '+7 (905) 696-55-91');
+//            phoneformat('8 495 941 93 20', '8 (495) 941-93-20');
+            phoneformat('', '');
+            phoneformat('', '');
+            phoneformat('', '');
+            phoneformat('', '');
+            phoneformat('', '');
+        });
+
+        
+    });
 });
              
